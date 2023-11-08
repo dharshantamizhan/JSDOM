3a81ee83faf565353ed4b120ad4472ad26ce5a41
sudharshan <sudharshan1903@gmail.com>
2023-11-08 21:36:08 +0530

    alignment changes

--- script.js
+++ script.js
@@ -16,16 +16,20 @@ setInterval(()=>{
         pmspan.className="pm active"
         amspan.className = "am"
         if(hours>12){
-            hoursspan.innerText = hours-12
+            hoursspan.innerText = (hours-12).toString().padStart(2,0);
+        }
+        if(hours==12)
+        {
+           hoursspan.innerText=(12).toString().padStart(2,0);
         }
         else{
-            hoursspan.innerText = hours
+            hoursspan.innerText = (hours).toString().padStart(2,0);
         }
     }
     else{
         pmspan.className="pm "
         amspan.className = "am active"
     }   
-    secspan.innerText=secs
-    minspan.innerText=minutes
+    secspan.innerText=secs.toString().padStart(2,0);
+    minspan.innerText=minutes.toString().padStart(2,0);
 })
